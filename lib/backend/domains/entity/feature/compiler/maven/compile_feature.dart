e377cba73cf8967c435d252c596299589f8d2557
Charles-Neyrand <77964361+Charles-Neyrand@users.noreply.github.com>
2023-06-23 12:17:23 +0200

    17 implement compilation features (#24)
    * feature: maven and tiger
    
    * Fix: changed passing parameters to named parameter
    
    * Merge Feature into implement (#23)
    
    * Feat: open project from starter page (#19)
    
    * feat: added file picker to choose directory, error in node service
    
    * feat: open project in starter page done
    
    * feat: add Feature interface (#22)
    
    ---------
    
    Co-authored-by: Barbora Plašovská <54357674+barboraplasovska@users.noreply.github.com>
    Co-authored-by: lucmahoux <68559909+lucmahoux@users.noreply.github.com>
    
    * Feat: implemented feature class
    
    * feature: tigrou feature
    
    ---------
    
    Co-authored-by: arnaud.lecoq <arnaud.lecoq@epita.fr>
    Co-authored-by: Barbora Plašovská <54357674+barboraplasovska@users.noreply.github.com>
    Co-authored-by: lucmahoux <68559909+lucmahoux@users.noreply.github.com>

---
+++ lib/backend/domains/entity/feature/compiler/maven/compile_feature.dart
@@ -0,0 +1,18 @@
+import 'package:pingfrontend/backend/domains/entity/feature/feature.dart';
+import 'package:pingfrontend/backend/domains/entity/project_interface.dart';
+
+import 'maven_compiler.dart';
+
+class CompileFeature extends Feature{
+
+  CompileFeature() : super(MavenFeature.compile);
+
+  Future<ExecutionReport> compile(IProject project, List<String> additionalArguments) async {
+    return await MavenCompiler.compile(project, 'compile', additionalArguments: additionalArguments);
+  }
+
+  @override
+  Future<ExecutionReport> execute(IProject project, {List<String> additionalArguments = const []} ) async {
+    return await compile(project, additionalArguments);
+  }
+}
