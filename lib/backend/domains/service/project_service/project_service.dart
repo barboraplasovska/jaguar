a59eade775a5272554f4bb74cae795415e676b9a
Luc Mahoux <luc.mahoux@epita.fr>
2023-06-23 16:31:13 +0200

    feat: load maven aspect in project, tigrou by default

--- lib/backend/domains/service/project_service/project_service.dart
+++ lib/backend/domains/service/project_service/project_service.dart
@@ -1,4 +1,8 @@
 
+import 'dart:core';
+import 'dart:io';
+
+import 'package:pingfrontend/backend/domains/entity/aspect/aspect.dart';
 import 'package:pingfrontend/backend/domains/entity/aspect_interface.dart';
 import 'package:pingfrontend/backend/domains/entity/node_interface.dart';
 import 'package:pingfrontend/backend/domains/entity/project/project.dart';
@@ -15,9 +19,26 @@ class  ProjectService implements IProjectService {
     return _nodeService;
   }
 
+  static bool _isMavenProject(String path) {
+      String fullPathToPom = "$path/pom.xml";
+      return File(fullPathToPom).existsSync();
+  }
+
+  Future<Set<IAspect>> _loadAspects(String path) async {
+    Set<IAspect> aspects = {};
+
+    if (_isMavenProject(path)) {
+      aspects.add(Aspect(AspectType.maven));
+    } else {
+      aspects.add(Aspect(AspectType.tigrou));
+    }
+
+    return aspects;
+  }
+
   @override
   IProject load(String rootPath) {
-    Set<IAspect> aspects = {};
+    Set<IAspect> aspects = _loadAspects(rootPath) as Set<IAspect>;
 
     INode rootNode = _nodeService.create(null, rootPath, NodeType.folder);
 
