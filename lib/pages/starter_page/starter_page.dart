bf9d582cbf8954ce944a13431f513b829358fdc7
USSRoosevelt <alexandra.delin@epita.fr>
2023-06-29 10:11:53 +0200

    finished : improving starter page

--- lib/pages/starter_page/starter_page.dart
+++ lib/pages/starter_page/starter_page.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
-
+import 'package:pingfrontend/backend/domains/entity/project_interface.dart';
 import 'package:pingfrontend/backend/domains/service/node_service/node_service.dart';
 import 'package:pingfrontend/pages/code_editor/code_editor_page.dart';
+import 'package:provider/provider.dart';
 
 import 'package:file_picker/file_picker.dart';
 
 import '../../backend/domains/service/project_service/project_service.dart';
+import '../../themes/theme_switcher.dart';
 
 class StarterPage extends StatelessWidget {
   const StarterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeSwitcher = Provider.of<ThemeSwitcher>(context);
     NodeService nodeService = NodeService();
     ProjectService projectService = ProjectService(nodeService);
     String? result;
 
     return Scaffold(
-      body: Center(
+      body: Container(
+        alignment: Alignment.center,
+         decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/braises-transformed.jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Padding(
               padding: EdgeInsets.all(10),
-              child: Text("Welcome to our super chuper IDE."),
+              child: Text("Welcome to our super chuper IDE.",
+              style: TextStyle(fontSize: 25)),
             ),
             Padding(
               padding: const EdgeInsets.all(10),
@@ -41,7 +54,11 @@ class StarterPage extends StatelessWidget {
                       )
                     },
                 },
-                child: const Text("Open project"),
+                 style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary),
+                child: const Text(
+                  "Open project",
+                  style: TextStyle(fontSize: 25),)
               ),
             ),
           ],
