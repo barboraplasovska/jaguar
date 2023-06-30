ed2654937be074c6a63948da96bb6417ae014818
Barbora Plasovska <barunka.plasovska@gmail.com>
2023-06-30 11:42:56 +0200

    feat: file tree is now horizontally scrollable

--- lib/components/file_tree/file_tree.dart
+++ lib/components/file_tree/file_tree.dart
@@ -13,21 +13,64 @@ class FileTree extends StatelessWidget {
     required this.root,
   });
 
+  double calculateFileTreeWidth(INode node, double level) {
+    double totalWidth = 0;
+
+    if (node.isFolder()) {
+      List<INode> sortedChildren = List.from(node.getChildren());
+      sortedChildren.sort((a, b) => a.getName().compareTo(b.getName()));
+
+      if (sortedChildren.isNotEmpty) {
+        INode lastChild = sortedChildren.last;
+        totalWidth += calculateFileTreeWidth(lastChild, level + 1) + 25;
+      }
+
+      totalWidth += sortedChildren.length * 25;
+    } else {
+      double textWidth = getTextWidth(node.getName());
+      totalWidth += textWidth;
+    }
+
+    return totalWidth;
+  }
+
+  double getTextWidth(String text) {
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: text,
+      ),
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout();
+
+    return textPainter.width;
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = 300;
+    double calculated = calculateFileTreeWidth(root, 0) + 100;
+    width = width < calculated ? calculated : width;
+
     return Consumer<FileProvider>(
       builder: (context, fileProvider, _) {
         return ChangeNotifierProvider<FileProvider>.value(
           value: fileProvider,
-          child: TreeView(
-            root: root,
-            onFileSelected: (file) {
-              final fileProvider = Provider.of<FileProvider>(
-                context,
-                listen: false,
-              );
-              fileProvider.selectedFile = file;
-            },
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: SizedBox(
+              width: width,
+              child: TreeView(
+                root: root,
+                onFileSelected: (file) {
+                  final fileProvider = Provider.of<FileProvider>(
+                    context,
+                    listen: false,
+                  );
+                  fileProvider.selectedFile = file;
+                },
+              ),
+            ),
           ),
         );
       },
