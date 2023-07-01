ea5388bcee67b26824910730d1871b0046f3c0ef
Barbora Plašovská <54357674+barboraplasovska@users.noreply.github.com>
2023-07-01 22:58:24 +0200

    feat: starter page (#68)
    * feat: starter page looks pretty cool
    
    * fixes

--- lib/components/buttons/open_project_button.dart
+++ lib/components/buttons/open_project_button.dart
@@ -4,6 +4,7 @@ import 'package:ping/backend/domains/entity/aspect_interface.dart';
 import 'package:ping/backend/domains/entity/project_interface.dart';
 import 'package:ping/backend/domains/service/node_service/node_service.dart';
 import 'package:ping/backend/domains/service/project_service/project_service.dart';
+import 'package:ping/backend/domains/service/shared_prefs_handler.dart';
 import 'package:ping/themes/theme_switcher.dart';
 
 import '../../pages/code_editor/code_editor_page.dart';
@@ -66,17 +67,18 @@ class _OpenProjectButtonState extends State<OpenProjectButton> {
   Function() buildOnPressed(BuildContext context) {
     return () async => {
           result = await FilePicker.platform.getDirectoryPath(),
-          project = projectService.load(result!),
-          widget.themeSwitcher.switchTheme(setProjectTheme(project)),
           if (result != null)
             {
+              project = projectService.load(result!),
+              widget.themeSwitcher.switchTheme(setProjectTheme(project)),
+              await addPreviousProject(project.getRootNode().getPath()),
               if (widget.pushReplacement)
                 {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
                       builder: (context) => CodeEditorPage(
-                        project: projectService.load(result!),
+                        project: project,
                       ),
                     ),
                   )
@@ -87,7 +89,7 @@ class _OpenProjectButtonState extends State<OpenProjectButton> {
                     context,
                     MaterialPageRoute(
                       builder: (context) => CodeEditorPage(
-                        project: projectService.load(result!),
+                        project: project,
                       ),
                     ),
                   )
