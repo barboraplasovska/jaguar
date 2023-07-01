0883754ebb45e877b514cebd46263836e9c1e80d
Barbora Plasovska <barunka.plasovska@gmail.com>
2023-07-01 19:55:58 +0200

    feat: added the possibility to change compilation options

--- lib/backend/domains/service/shared_prefs_handler.dart
+++ lib/backend/domains/service/shared_prefs_handler.dart
@@ -11,3 +11,27 @@ void setTigerPath(String path) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   await prefs.setString("tigerPath", path);
 }
+
+Future<String> getTigerCompilationOptions() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String? path = prefs.getString('tigerCompilationOptions');
+  return path ?? "";
+}
+
+void setTigerCompilationOptions(String path) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString("tigerCompilationOptions", path);
+}
+
+Future<String> getJavaCompilationOptions() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String? path = prefs.getString('javaCompilationOptions');
+  return path ?? "";
+}
+
+void setJavaCompilationOptions(String path) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString("javaCompilationOptions", path);
+}
