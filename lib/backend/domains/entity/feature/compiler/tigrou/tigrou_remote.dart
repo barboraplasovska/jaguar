ba0a7faa7e981629f8b9abdf3833f264958ff2d3
lucmahoux <68559909+lucmahoux@users.noreply.github.com>
2023-07-05 20:35:46 +0200

    99 feat add backend logic of remote compiler (#104)
    * build: add http dependency
    
    * feat: add tiger remote compiler
    
    * tests: add tiger remote compiler unit tests
    
    * feat: add handling of input files in remote compiler execution

---
+++ lib/backend/domains/entity/feature/compiler/tigrou/tigrou_remote.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ping/backend/domains/entity/feature/feature.dart';
+import 'package:ping/backend/domains/entity/project_interface.dart';
+import 'package:http/http.dart' as http;
+import 'package:ping/backend/utils/files/file_utils.dart';
+
+class ServerResponse {
+  final String content;
+
+  ServerResponse({required this.content});
+
+  factory ServerResponse.fromJson(Map<String, dynamic> json) {
+    return ServerResponse(
+      content: json['output'],
+    );
+  }
+}
+
+class TigrouRemote extends Feature {
+  TigrouRemote() : super(TigerFeature.remote);
+
+  Future<ServerResponse> remoteCompile(String content) async {
+    var data = {
+      'content': content,
+    };
+    final response =  await http.post(
+      Uri.parse('https://tigrou.celian.cloud/compile'),
+      body: data,
+    );
+
+    if (response.statusCode == 200) {
+      return ServerResponse.fromJson(jsonDecode(response.body));
+    } else {
+      return ServerResponse.fromJson(jsonDecode(response.body));
+    }
+  }
+
+
+  @override
+  Future<ExecutionReport> execute(IProject project, {List<String> additionalArguments = const []}) async {
+    String input = await File(additionalArguments[0]).readAsString();
+
+    ServerResponse response = await remoteCompile(input);
+    await writeOutput(response.content, project.getRootNode().getPath());
+    ExecutionReport report = () => true;
+
+    return Future.value(report);
+  }
+
+}
---
+++ test/remote_compiler_test.dart
@@ -0,0 +1,49 @@
+
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ping/backend/domains/entity/feature/compiler/tigrou/tigrou_compile_feature.dart';
+import 'package:ping/backend/domains/entity/feature/compiler/tigrou/tigrou_remote.dart';
+
+void main() {
+  group('Remote Tiger Compiler', () {
+
+    test('Hello World', () async {
+      String input = await File('test/tiger_input/hello.tig').readAsString();
+
+      TigrouRemote tigrouRemote = TigrouRemote();
+
+      var response = await tigrouRemote.remoteCompile(input);
+      expect(response.content, equals("Hello World\n"));
+    });
+
+    test('If', () async {
+      String input = await File('test/tiger_input/if.tig').readAsString();
+
+      TigrouRemote tigrouRemote = TigrouRemote();
+
+      var response = await tigrouRemote.remoteCompile(input);
+      expect(response.content, equals("40"));
+    });
+
+    test('Fact', () async {
+      String input = await File('test/tiger_input/fact.tig').readAsString();
+
+      TigrouRemote tigrouRemote = TigrouRemote();
+
+      var response = await tigrouRemote.remoteCompile(input);
+      expect(response.content, equals("3628800\n"));
+    });
+
+    test('Record', () async {
+      String input = await File('test/tiger_input/record.tig').readAsString();
+
+      TigrouRemote tigrouRemote = TigrouRemote();
+
+      var response = await tigrouRemote.remoteCompile(input);
+      expect(response.content, equals("Somebody\n1000"));
+    });
+  });
+}
